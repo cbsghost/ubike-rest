ec85f6b39bc558b0c22bca751f431910998ea755
Andy Liao <499750058@ntnu.edu.tw>
2017-04-28 07:15:46 +0800

    Remove cache middleware

--- routes/my-api.js
+++ routes/my-api.js
@@ -1,7 +1,5 @@
 var express = require('express');
 var router = express.Router();
-var apicache = require('apicache');
-var cache = apicache.middleware;
 
 var request = require('request');
 var zlib = require('zlib');
@@ -17,7 +15,7 @@ function ubikeRequest(times) {
         ubike_json = null;
         return;
     }
-    
+
     var options = {
         followAllRedirects: true,
         timeout: 10000,
@@ -28,7 +26,7 @@ function ubikeRequest(times) {
         },
         encoding: null
     };
-    
+
     request.get(options, function (error, response, body) {
         if (!error && response.statusCode === 200) {
             var encoding = response.headers['content-encoding']
@@ -57,18 +55,18 @@ setTimeout(ubikeInterrupt, 60*1000);
 
 function calcNearestStation(lat, lng) {
     var candidate_list = [];
-    
+
     if (ubike_json === null || !ubike_json["retVal"]) {
         return undefined;
     }
-    
+
     for(var key in ubike_json["retVal"]){
         if (!ubike_json["retVal"][key]["sbi"] || ubike_json["retVal"][key]["sbi"] <= 0) {
             continue;
         }
-        
+
         var candidate = {};
-        
+
         var sta_lat = ubike_json["retVal"][key]["lat"];
         var sta_lng = ubike_json["retVal"][key]["lng"];
         if (isNaN(sta_lat) || isNaN(sta_lng)) {
@@ -76,30 +74,30 @@ function calcNearestStation(lat, lng) {
         }
         sta_lat = parseFloat(sta_lat);
         sta_lng = parseFloat(sta_lng);
-    
+
         // delete the sqrt calculation to accelerate
         candidate[key] = Math.pow(sta_lat - lat ,2) + Math.pow(sta_lng - lng ,2);
         candidate_list.push(candidate);
     }
-    
+
     if (candidate_list.length === 0) {
         return null;
     } else if (candidate_list.length === 1) {
         return [Object.keys(candidate_list[0])[0]];
     }
-    
+
     candidate_list.sort(function(a, b) {
         return a[Object.keys(a)[0]] - b[Object.keys(b)[0]];
     });
     return [Object.keys(candidate_list[0])[0], Object.keys(candidate_list[1])[0]];
 }
 
-router.get('/taipei', cache('1 minutes'), function(req, res) {
+router.get('/taipei', function(req, res) {
     var res_json = {
         "code": 0,
         "result": []
     };
-    
+
     // check valid coordinate
     var lat = req.query.lat;
     var lng = req.query.lng;
@@ -115,7 +113,7 @@ router.get('/taipei', cache('1 minutes'), function(req, res) {
         res.json(res_json);
         return;
     }
-    
+
     // check is in Taipei
     geocoder.reverse(lat, lng)
         .then(function(result) {
@@ -124,7 +122,7 @@ router.get('/taipei', cache('1 minutes'), function(req, res) {
                 res.json(res_json);
                 return;
             }
-                
+
             // calculate the distance
             var sta_list = calcNearestStation(lat, lng);
             var result = {};
@@ -140,7 +138,7 @@ router.get('/taipei', cache('1 minutes'), function(req, res) {
                 res.json(res_json);
                 return;
             }
-                
+
             for (i in sta_list) {
                 result = {};
                 result["station"] = ubike_json["retVal"][sta_list[i]]["sna"];
